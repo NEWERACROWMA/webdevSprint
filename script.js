c3e4c8026c4b633e0b6fe6df0c240a8d943b4070
Gabriel Lilla <gabrielterralilla@gmail.com>
2024-06-16 04:42:15 -0300

    att

--- script.js
+++ script.js
@@ -10,6 +10,52 @@ document.addEventListener("DOMContentLoaded", function() {
         slideIndex++;
         if (slideIndex > slides.length) { slideIndex = 1; }
         slides[slideIndex - 1].classList.add('active');
-        setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
+        setTimeout(showSlides, 3000); 
+    }
+
+    const loginForm = document.getElementById("loginForm");
+
+    if (loginForm) {
+        loginForm.addEventListener("submit", function(event) {
+            event.preventDefault(); 
+
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+
+            if (username === "newUsername" && password === "newPassword") {
+                alert("Login realizado com sucesso!");
+                window.location.href = "index.html";
+            } else {
+                alert("Usuário ou senha incorretos. Tente novamente.");
+            }
+        });
+    }
+
+    const signupForm = document.getElementById("signupForm");
+
+    if (signupForm) {
+        signupForm.addEventListener("submit", function(event) {
+            event.preventDefault(); 
+
+            const newUsername = document.getElementById("newUsername").value;
+            const newPassword = document.getElementById("newPassword").value;
+
+            if (newUsername && newPassword) {
+                alert("Usuário registrado com sucesso! Agora faça login.");
+                document.getElementById("newUsername").value = "";
+                document.getElementById("newPassword").value = "";
+                window.location.href = "login.html";
+            } else {
+                alert("Por favor, preencha todos os campos.");
+            }
+        });
+    }
+
+    const alertButton = document.getElementById("alertButton");
+
+    if (alertButton) {
+        alertButton.addEventListener("click", function() {
+            alert("Esse é um alerta!");
+        });
     }
 });
