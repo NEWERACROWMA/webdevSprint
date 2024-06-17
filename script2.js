47298209bbfaf242bfa87e06299d3347d601eec1
Gabriel Lilla <gabrielterralilla@gmail.com>
2024-06-16 22:14:52 -0300

    att fix

---
+++ script2.js
@@ -0,0 +1,52 @@
+const signupForm = document.getElementById("signupForm");
+
+if (signupForm) {
+    signupForm.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+        const newUsername = document.getElementById("newUsername").value;
+        const newPassword = document.getElementById("newPassword").value;
+
+        if (newUsername && newPassword) {
+            localStorage.setItem("username", newUsername);
+            localStorage.setItem("password", newPassword);
+
+            alert("Usuário registrado com sucesso! Agora faça login.");
+            document.getElementById("newUsername").value = "";
+            document.getElementById("newPassword").value = "";
+            window.location.href = "login.html";
+        } else {
+            alert("Por favor, preencha todos os campos.");
+        }
+    });
+}
+
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        const storedUsername = localStorage.getItem("username");
+        const storedPassword = localStorage.getItem("password");
+
+        if (username === storedUsername && password === storedPassword) {
+            alert("Login realizado com sucesso!");
+            window.location.href = "index.html";
+        } else {
+            alert("Usuário ou senha incorretos. Tente novamente.");
+        }
+    });
+}
+
+
+const alertButton = document.getElementById("alertButton");
+
+if (alertButton) {
+    alertButton.addEventListener("click", function() {
+        alert("Esse é um alerta!");
+    });
+}
